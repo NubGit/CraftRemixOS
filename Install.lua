e7825c462e596a0ab1d6e4281266d635e6899d7a
NubGit <47465990+NubGit@users.noreply.github.com>
2019-02-09 17:15:39 -0500

    Update Install.lua

--- Install.lua
+++ Install.lua
@@ -1,16 +1,10 @@
 
-local file = startup.lua
-local url = https://raw.githubusercontent.com/NubGit/CraftRemixOS/master/Main.lua
 --## Remix Craft Os ##--
 -- THIS OPERATING SYSTEM IS FREE TO MODIFY BUT DO NOT REPOST. ALWAYS SHARE PROJECT TO https://github.com/NubGit/CraftRemixOS --
 term.setCursorPos(1,1)
 term.clear()
-print("Setup")
-print("Welcome to Remix Craft os! to install please enter Y/N")
-local choice = io.read()
-if choice == Y then
-term.clear()
 term.setCursorPos(1,1)
+term.clear()
 print("THIS OPERATING SYSTEM IS FREE TO MODIFY BUT DO NOT REPOST. ALWAYS SHARE PROJECT TO https://github.com/NubGit/CraftRemixOS")
 sleep(5)
 term.clear()
@@ -21,12 +15,9 @@ shell.run("wget https://raw.githubusercontent.com/NubGit/CraftRemixOS/master/Mai
 term.clear()
 print("Installation Finished, want to reboot? [Y/N]")
 term.setCursorPos(1,5)
-local choice1 = io.read()
-if choice1 == Y then
+local rebootn = read()
+if rebootn == Y then
 os.reboot()
-else
-error()
-end
 else
 error()
 end
